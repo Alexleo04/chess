4bcdc95e489562cf82014734a1b7c850229cfdbf
Alexleo04 <45465859+Alexleo04@users.noreply.github.com>
2020-12-04 18:46:53 +0100

    Initial Commit

---
+++ Real_Chess/Real_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_ChessApp.swift
+//  Real_Chess
+//
+//  Created by Aleksey Leonov on 04/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Real_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
